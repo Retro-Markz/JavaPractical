c0b56931047e3410045f3e60f29394aa47fc6461
markz <cubedomarco@gmail.com>
2024-09-14 05:55:49 -0700

    start functions body

---
+++ src/Functions.java
@@ -0,0 +1,10 @@
+public class Functions {
+
+    static void sayHello(String message){
+        System.out.println("Message: " + message);
+    }
+    public static void main(String[] args) {
+            sayHello("Hello mates! i'm on Java");
+            sayHello("Well, nothing more to say, good bye");
+    }
+}
